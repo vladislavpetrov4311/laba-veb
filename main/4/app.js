208928fedd62f316f81e50ca5df01d8d3823177f
vladislavpetrov4311 <103639622+vladislavpetrov4311@users.noreply.github.com>
2022-04-14 13:54:29 +0300

    добавил 4-ую лабораторную
    ...

---
+++ main/4/app.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var toDos = [
+"Закончить писать эту книгу",
+"Вывести Грейси на прогулку в парк", 
+"Ответить на электронные письма", 
+"Подготовиться к лекции в понедельник", 
+"Обновить несколько новых задач", 
+"Купить продукты"
+];
+
+$("document").ready( function(){
+
+$(".tabs a span").toArray().forEach(function (element) { 
+	// создаем обработчик щелчков для этого элемента 
+	$(element).on("click", function () {
+		// поскольку мы используем версию элемента jQuery,
+		// нужно создать временную переменную,
+		// чтобы избежать постоянного обновления
+		var $element = $(element);
+		$(".tabs a span").removeClass("active"); 
+		$(element).addClass("active");
+		$("main .content").empty();
+
+		if ($element.parent().is(":nth-child(1)")) { 
+			for (var i = toDos.length-1; i > -1; i--) { 
+				$(".content").append($("<li>").text(toDos[i]));
+			}
+		} 
+		else if ($element.parent().is(":nth-child(2)")) { 
+			toDos.forEach(function (todo) { 
+				$(".content").append($("<li>").text(todo));
+			});
+		} 
+		else if ($element.parent().is(":nth-child(3)")) { 
+			$(".content").append(
+				'<input type="text" class="inp">'+
+				'<button class="btn">Добавить</button>'
+			);
+			var newToDo;
+			$('.btn').on('click',function(){
+				newToDo= $('.inp').val();
+				if (newToDo!='') {
+					toDos.push( newToDo);
+					alert('Новое задание "'+newToDo+'" успешно добавлено!');
+					$('.inp').val("");
+				}
+			})
+		
+		}
+		return false;
+	})
+})
+
+
+$(".tabs a:first-child span").trigger("click");
+
+})
